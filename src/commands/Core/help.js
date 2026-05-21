96b84e01e97770a95c5b115043609ac43287f558
xladiesman217x <hidden.stx.ink@gmail.com>
2026-05-21 06:31:41 -0500

    Update footer text in help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -158,7 +158,7 @@ export async function createInitialHelpMenu(client) {
     );
 
     embed.setFooter({ 
-        text: "Made with ❤️" 
+        text: "Made by J. Martin " 
     });
     embed.setTimestamp();
 
