91437298afa6ebecb694acf9ce66aceb1debc88a
xladiesman217x <hidden.stx.ink@gmail.com>
2026-06-14 01:29:37 -0500

    Update bot activity name to 'Priority 1 Developer'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Developing Idaho State RP",
+        name: "Priority 1 Developer",
         // Activity type number (0 = Playing).
         type: 0, 
       },
